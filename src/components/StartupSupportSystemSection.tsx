1f114d54e7f0787c80eb781006d065d178999192
주강 <jkpark@nkia.co.kr>
2025-10-08 20:55:23 +0900

    fix: 불필요 코드 삭제

--- src/components/StartupSupportSystemSection.tsx
+++ src/components/StartupSupportSystemSection.tsx
@@ -1,7 +1,6 @@
 "use client";
 
 import { motion } from "framer-motion";
-import startupSupportSystemData from "@/mock/startupSupportSystemData";
 
 const StartupSupportSystemSection = () => {
   return (
